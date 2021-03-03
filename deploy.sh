cfee600e90c20c982037713437e96a5f9078ae00
Chunpeng Huo <huocp@me.com>
2021-03-03 18:57:02 +1100

    chore: update instruction

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ echo "Deploy files to Digital Ocean ..."
 ./copy-to-server.sh
 
 echo "Deployed!"
-echo "To restart cache or oauth server, ssh to dumber.app and touch server/dumber-cache/tmp/restart.txt and server/dumber-cache/tmp/restart.txt"
+echo "To restart cache or oauth server, ssh to dumber.app and touch dumber-gist/server/dumber-cache/tmp/restart.txt and dumber-gist/server/github-oauth/tmp/restart.txt"
